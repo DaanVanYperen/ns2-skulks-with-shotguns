b9c42495a2b43210702cf032bef303d378883179
Daan <daan@mostlyoriginal.net>
2014-01-05 20:30:04 +0100

    + Embedded new Ikea and Refinery maps for now. + Increased starting eggs to 60

--- output/lua/SWS_Shared.lua
+++ output/lua/SWS_Shared.lua
@@ -12,7 +12,7 @@ Script.Load("lua/SWS_Locale.lua")
     end      
 
 // number of eggs that can spawn in one round.
-kPlayingTeamInitialTeamRes = 2
+kPlayingTeamInitialTeamRes = 60
 kTeamResourcePerTick = 0
 
 kTeam1Name = "Shadow"
