cc9a85335020b18f7005a9f7ed23bd4f0ca831a6
Daan <daan@mostlyoriginal.net>
2014-01-05 21:34:51 +0100

    Updated locale messages.

--- output/lua/SWS_Locale.lua
+++ output/lua/SWS_Locale.lua
@@ -1,12 +1,14 @@
 
 kCustomLocaleMessages = {
     HIVE = 'Stalk',
-    HIVE_TOOLTIP = 'Produces eggs while alive.',
-    HIVE_HINT = 'Produces eggs while alive.',
+    HIVE_TOOLTIP = 'Produces eggs for your team.',
+    HIVE_HINT = 'Produces eggs for your team.',
     TEAM_RES = 'EGG RESERVE: %d',
     ALIEN_TEAM_GAME_STARTED = 'Objective: Kill all opposition',
     RESOURCE_NOZZLE = 'Dead Stalk',
     RESOURCE_NOZZLE_TOOLTIP = 'No more eggs can span here.',
+    ALIEN_ALERT_HIVE_DYING = 'Stalk is dying',
+    ALIEN_ALERT_HIVE_UNDERATTACK = 'Stalk under attack',
 }
 
 if Locale then
