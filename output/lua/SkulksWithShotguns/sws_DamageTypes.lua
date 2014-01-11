cff5c99136621ff394ab1775747126a3e44bc25d
Daan <daan@mostlyoriginal.net>
2014-01-12 00:21:37 +0100

    + Shotties are tech 3 while under fire effects.

--- output/lua/SkulksWithShotguns/sws_DamageTypes.lua
+++ output/lua/SkulksWithShotguns/sws_DamageTypes.lua
@@ -2,3 +2,21 @@
 // deal full damage to 'friendlies'
 kFriendlyFireScalar = 1
 
+
+// Use this function to change damage according to current upgrades
+function NS2Gamerules_GetUpgradedDamage(attacker, doer, damage, damageType, hitPoint)
+
+    local damageScalar = 1
+
+    if attacker ~= nil then
+    
+        // Boost damage 
+        if HasMixin(attacker, "Fire") and attacker:GetIsOnFire() then
+           damageScalar = kWeapons3DamageScalar 
+        end
+        
+    end
+        
+    return damage * damageScalar
+    
+end
