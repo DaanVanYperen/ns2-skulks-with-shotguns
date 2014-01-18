7cd09e0ed361782f00aea259cd97c08de63ca94d
Daan <daan@mostlyoriginal.net>
2014-01-18 01:40:41 +0100

    Sound effects.

--- output/lua/SkulksWithShotguns/sws_Client.lua
+++ output/lua/SkulksWithShotguns/sws_Client.lua
@@ -1,6 +1,6 @@
 decoda_name = "Client"
 Script.Load("lua/PreLoadMod.lua")
 
-Shared.Message('Skulks With Shotguns [EARLY ALPHA] V0.0.11')
+Shared.Message('Skulks With Shotguns [EARLY ALPHA] V0.0.12')
 
 Script.Load("lua/PostLoadMod.lua") 
