cac932993f1002783b7c5ddae9cfc96c7d76e9b1
Daan <daan@mostlyoriginal.net>
2014-01-18 19:51:53 +0100

    + Linked sounds to sfx volume. No more yelling!

--- output/lua/SkulksWithShotguns/sws_Sounds.lua
+++ output/lua/SkulksWithShotguns/sws_Sounds.lua
@@ -1,9 +1,9 @@
 
-kSfxGorgeStolenOur = PrecacheAsset("sound/sws.fev/untitled/sws/gorge_stolen_our")
-kSfxGorgeStolenTheir = PrecacheAsset("sound/sws.fev/untitled/sws/gorge_stolen_their")
+kSfxGorgeStolenOur = PrecacheAsset("sound/sws2.fev/common/gorge_stolen_our")
+kSfxGorgeStolenTheir = PrecacheAsset("sound/sws2.fev/common/gorge_stolen_their")
 
-kSfxBlueScores = PrecacheAsset("sound/sws.fev/untitled/sws/blue_scores")
-kSfxRedScores = PrecacheAsset("sound/sws.fev/untitled/sws/red_scores")
+kSfxBlueScores = PrecacheAsset("sound/sws2.fev/common/blue_scores")
+kSfxRedScores = PrecacheAsset("sound/sws2.fev/common/red_scores")
 
-kSfxBlueWins = PrecacheAsset("sound/sws.fev/untitled/sws/blue_wins")
-kSfxRedWins = PrecacheAsset("sound/sws.fev/untitled/sws/red_wins")
+kSfxBlueWins = PrecacheAsset("sound/sws2.fev/common/blue_wins")
+kSfxRedWins = PrecacheAsset("sound/sws2.fev/common/red_wins")
