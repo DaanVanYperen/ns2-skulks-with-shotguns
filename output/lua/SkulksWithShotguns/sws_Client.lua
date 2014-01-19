f5f1dcf9d6e259bfa9795fa0c51b726378afdbf8
Daan <daan@mostlyoriginal.net>
2014-01-19 16:11:36 +0100

    Version bump to 0.0.14

--- output/lua/SkulksWithShotguns/sws_Client.lua
+++ output/lua/SkulksWithShotguns/sws_Client.lua
@@ -1,6 +1,6 @@
 decoda_name = "Client"
 Script.Load("lua/PreLoadMod.lua")
 
-Shared.Message('Skulks With Shotguns [EARLY ALPHA] V0.0.13')
+Shared.Message('Skulks With Shotguns [EARLY ALPHA] V0.0.14')
 
 Script.Load("lua/PostLoadMod.lua") 
