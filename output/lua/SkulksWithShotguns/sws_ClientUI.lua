d768d27339c0f0bc07628d138ea25ede772f6cb9
Daan <daan@mostlyoriginal.net>
2014-02-02 17:52:29 +0100

    Spedometer prep.

--- output/lua/SkulksWithShotguns/sws_ClientUI.lua
+++ output/lua/SkulksWithShotguns/sws_ClientUI.lua
@@ -6,8 +6,8 @@ local kShowOnTeam = GetLocalFunction(RemoveScripts, 'kShowOnTeam')
 // information not needed for our gamemode.
 kShowAsClass["Alien"].GUIBioMassDisplay = false
 kShowAsClass["Alien"].GUIUpgradeChamberDisplay = false
-kShowAsClass["Alien"].GUI = false
 
+//kShowAsClass["Alien"]["GUISpeedometer"] = true
 kShowAsClass["Alien"]["GUIFlagScore"] = true
 kShowAsClass["AlienSpectator"]["GUIFlagScore"] = true
 
@@ -15,3 +15,5 @@ kShowOnTeam[kTeam1Index]["GUIAlienSpectatorHUD"] = true
 kShowOnTeam[kTeam2Index]["GUIAlienSpectatorHUD"] = true
 kShowOnTeam[kTeam1Index]["GUIFlagScore"] = true
 kShowOnTeam[kTeam2Index]["GUIFlagScore"] = true
+//kShowOnTeam[kTeam1Index]["GUISpeedometer"] = true
+//kShowOnTeam[kTeam2Index]["GUISpeedometer"] = true
