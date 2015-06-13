6657308cf08f9fdbd80b40d3e95f88da30477689
DaanVanYperen <daan@mostlyoriginal.net>
2015-06-13 15:21:00 +0200

    v0.18 fixes for 274.

--- output/lua/SkulksWithShotguns/sws_Client.lua
+++ output/lua/SkulksWithShotguns/sws_Client.lua
@@ -1,6 +1,6 @@
 decoda_name = "Client"
 Script.Load("lua/PreLoadMod.lua")
 
-Shared.Message('Skulks With Shotguns [ALPHA] V0.0.17')
+Shared.Message('Skulks With Shotguns [ALPHA] V0.0.18')
 
 Script.Load("lua/PostLoadMod.lua") 
