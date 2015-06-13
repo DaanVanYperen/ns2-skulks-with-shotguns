b3989ff718cfc9c4db0bd9e235714c6ea633e5f1
DaanVanYperen <daan@mostlyoriginal.net>
2015-06-13 15:22:59 +0200

    Begin new development cycle.

--- output/lua/SkulksWithShotguns/sws_Client.lua
+++ output/lua/SkulksWithShotguns/sws_Client.lua
@@ -1,6 +1,6 @@
 decoda_name = "Client"
 Script.Load("lua/PreLoadMod.lua")
 
-Shared.Message('Skulks With Shotguns [ALPHA] V0.0.18')
+Shared.Message('Skulks With Shotguns [ALPHA] V0.0.19')
 
 Script.Load("lua/PostLoadMod.lua") 
